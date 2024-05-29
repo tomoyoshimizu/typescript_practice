62537f007b1f8d6e1d606ff7d2502c077d3305ff
tomoyoshimizu <observatory17@gmail.com>
2024-05-29 23:15:05 +0900

    [Finish] Chapter 5

---
+++ src/class_object.ts
@@ -0,0 +1,16 @@
+class CommonUser {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+  };
+
+  getMessage(message: string): string {
+    return `${this.name}(${this.age})「${message}」`;
+  }
+}
+
+const uhyo = new CommonUser("uhyo", 26);
+console.log(uhyo.getMessage("こんにちは"));
