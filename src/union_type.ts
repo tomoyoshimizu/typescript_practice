02505e41918bf5516825bdeef508089b305fe56e
tomoyoshimizu <observatory17@gmail.com>
2024-06-01 18:28:33 +0900

    [Finish] Chapter 6

---
+++ src/union_type.ts
@@ -0,0 +1,45 @@
+type Present<T> = {
+  hasValue: true;
+  value: T;
+};
+type Blank = {
+  hasValue: false;
+};
+type Option<T> = Present<T> | Blank;
+
+function showNumber(entry: Option<number>): void{
+  if (entry.hasValue){
+    console.log(entry.value);
+  }
+}
+
+function doubleNumber(entry: Option<number>){
+  return mapOption<number, number>(entry, (x) => x * 2);
+}
+
+function mapOption<T, U>(entry: Option<T>, callback: (value: T) => U): Option<U>{
+  if (entry.hasValue){
+    return {
+      hasValue: true,
+      value: callback(entry.value)
+    }
+  } else {
+    return {
+      hasValue: false
+    }
+  }
+}
+
+const myNumber: Option<number> = {
+  hasValue: true,
+  value: 896
+};
+
+const blankNumber: Option<number> = {
+  hasValue: false,
+};
+
+showNumber(myNumber);
+console.log(doubleNumber(myNumber));
+showNumber(blankNumber);
+console.log(doubleNumber(blankNumber));
