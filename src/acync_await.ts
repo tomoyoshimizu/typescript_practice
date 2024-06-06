bff766d116bc006ea2f39d705cca4e0967ae2e9e
tomoyoshimizu <observatory17@gmail.com>
2024-06-06 21:08:30 +0900

    [Finish] Chapter 8

---
+++ src/acync_await.ts
@@ -0,0 +1,26 @@
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration);
+  })
+};
+
+const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(reject, duration);
+  })
+};
+
+const p = Promise.race([
+  sleepReject(4000),
+  sleep(3000)
+]);
+
+p
+.then((result) => {
+  console.log("成功");
+})
+.catch((error: unknown) => {
+  console.log("失敗");
+})
+
+export {};
